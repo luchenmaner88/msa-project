889b0f9301c7967f5ef38cd86240ffa2572514b6
luchenmaner88 <119443857+luchenmaner88@users.noreply.github.com>
2024-09-10 17:44:17 -0500

    Update index.php

--- index.php
+++ index.php
@@ -5,42 +5,5 @@
 </head>
 <body>
 	<h1>MySQL Table Viewer</h1>
-	<?php
-		// Define database connection variables
-		$servername = "db-employee-mysql.mysql.database.azure.com";
-		$username = "mysqladmin";
-		$password = "9888210310@Cl";
-		$dbname = "employeedatabase";
-
-		// Create database connection
-		//$conn = new mysqli($servername, $username, $password, $dbname);
-                $conn = mysqli_init();
-                mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
-                mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, MYSQLI_CLIENT_SSL);
-
-		// Check connection
-		if ($conn->connect_error) {
-			die("Connection failed: " . $conn->connect_error);
-		}
-
-		// Query database for all rows in the table
-		$sql = "SELECT * FROM mytable";
-		$result = $conn->query($sql);
-
-		if ($result->num_rows > 0) {
-			// Display table headers
-			echo "<table><tr><th>ID</th><th>Name</th><th>Email</th></tr>";
-			// Loop through results and display each row in the table
-			while($row = $result->fetch_assoc()) {
-				echo "<tr><td>" . $row["id"] . "</td><td>" . $row["name"] . "</td><td>" . $row["email"] . "</td></tr>";
-			}
-			echo "</table>";
-		} else {
-			echo "0 results";
-		}
-
-		// Close database connection
-		$conn->close();
-	?>
 </body>
 </html>
