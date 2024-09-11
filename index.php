8f22ef1baade6f7be121d4f69dc3291409ad3828
luchenmaner88 <119443857+luchenmaner88@users.noreply.github.com>
2024-09-10 19:59:40 -0500

    Update index.php

--- index.php
+++ index.php
@@ -15,8 +15,8 @@
 		// Create database connection
 	 
 	        $conn = mysqli_init();
-                mysqli_ssl_set($conn,NULL,NULL, "{path to CA cert}", NULL, NULL);
-                mysqli_real_connect($conn, "db-employee-mysql.mysql.database.azure.com", "mysqladmin", $password, $dbname, 3306, MYSQLI_CLIENT_SSL);
+               // mysqli_ssl_set($conn,NULL,NULL, "{path to CA cert}", NULL, NULL);
+                mysqli_real_connect($conn, "db-employee-mysql.mysql.database.azure.com", "mysqladmin", $password, $dbname, 3306);
 
 		// Check connection
 		if ($conn->connect_error) {
